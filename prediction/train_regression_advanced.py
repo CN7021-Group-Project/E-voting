041026ba12bed3715c8381db4cc97f0e0e12994f
ganesh1997oli <42571338+ganesh1997oli@users.noreply.github.com>
2025-12-19 21:42:41 +0000

    add changes

--- prediction/train_regression_advanced.py
+++ prediction/train_regression_advanced.py
@@ -6,18 +6,22 @@ import pickle
 
 df = pd.read_csv("prediction/ml_turnout_features_clean.csv")
 
-# FEATURES TO HELP MODEL LEARN MORE
+# features to help model learn more
 df["votes_per_candidate"] = df["total_votes"] / df["num_candidates"]
-df["turnout_percentage"] = df["turnout_ratio"] * 100 # target in %
+# target in %
+df["turnout_percentage"] = df["turnout_ratio"] * 100 
 
 X = df[["num_candidates","duration_days","total_registered_voters","votes_per_candidate"]]
-y = df["turnout_percentage"]  # 🔥 regression target
+# regression target
+y = df["turnout_percentage"]  
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
 model = RandomForestRegressor(
-    n_estimators=350,       # stronger model
-    max_depth=None,         # let trees grow fully
+    # stronger model
+    n_estimators=350,     
+    # let trees grow fully  
+    max_depth=None,         
     min_samples_split=2,
     random_state=42
 )
@@ -26,9 +30,9 @@ model.fit(X_train, y_train)
 pred = model.predict(X_test)
 
 print("\n📊 MODEL PERFORMANCE")
-print("R² Score:", r2_score(y_test, pred))
+print("R² Score:", r2_score(y_test, pred)) # regression score function
 print("MAE:", mean_absolute_error(y_test, pred))
 
-# SAVE MODEL
+# save model
 pickle.dump(model, open("prediction/random_forest_turnout_reg.pkl","wb"))
 print("\n🔥 Regression Model Saved Successfully!")
